a62ba525c9dcbf586db83a80102b273db59dd24d
Haakon <enzigar1@gmail.com>
2024-06-06 21:42:36 +0200

    committing

--- src/Components/UncategorizedBudgetCard.js
+++ src/Components/UncategorizedBudgetCard.js
@@ -3,13 +3,11 @@ import BudgetCard from "./BudgetCard";
 
 export default function UncategorizedBudgetCard(props) {
 
-    const {getBudgetExpenses} = useBudgets()
-    const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce((total,
-        expense) => total + expense.amount,
-        0
-    )
-    if (amount === 0) return null
+    const { getBudgetExpenses } = useBudgets()
+    const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce 
+    ((total, expense) => total + expense.amount, 0 )
 
-    return <BudgetCard amount = {amount} name = "Uncategorized field" gray {...props}/>
+    if (amount === 0) return null
+    return <BudgetCard  amount = {amount} name = "uncategorized / unused" gray {...props}/>
 
 }
